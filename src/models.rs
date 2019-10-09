9a472c342e26ce4298a9f11fe6d497ff3d466c9a
Dani Pozo <danipozo@correo.ugr.es>
2019-10-09 20:05:58 +0200

    Añade test para consultar la tabla de ingredientes. Avanza #4

--- src/models.rs
+++ src/models.rs
@@ -11,3 +11,22 @@ pub struct Recipe {
     pub preparation_time: i32,
     pub main_ingredient: i32,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::diesel::prelude::*;
+    use crate::diesel::Connection;
+    use crate::schema::ingredient::dsl::*;
+
+    #[test]
+    fn query_ingredients() {
+        let conn = diesel::SqliteConnection::establish("test.sqlite")
+            .expect("Error connecting to database");
+
+        let _result = ingredient
+            .limit(5)
+            .load::<Ingredient>(&conn)
+            .expect("Error loading ingredients");
+    }
+}
