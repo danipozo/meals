efdda8055f97c4fcf73eaaa44c6669659b6dce40
Dani Pozo <danipozo@correo.ugr.es>
2019-10-25 18:42:41 +0200

    Añade datos de prueba y reubica migración. Avanza #9

--- migrations/2019-10-19-172833_add_test_data/up.sql
+++
@@ -1,5 +0,0 @@
-insert into ingredient(name, energy, carbohydrates, sugar, proteins, fat, ing_type) values ("Cebolla", 26, 5, null, 1, 0, 6);
-insert into ingredient(name, energy, carbohydrates, sugar, proteins, fat, ing_type) values ("Tomate", 19, 4, null, 1, 0, 5);
-insert into ingredient(name, energy, carbohydrates, sugar, proteins, fat, ing_type) values ("Pimiento rojo", 29, 5, null, 1, 0, 6);
-insert into ingredient(name, energy, carbohydrates, sugar, proteins, fat, ing_type) values ("Carne de cerdo", 124, 0, null, 25, 5, 1);
-
