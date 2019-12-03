5c4fdcd62876ec5d2457b155c80c6388d8aef5ce
Dani Pozo <danipozo@correo.ugr.es>
2019-12-03 23:27:51 +0100

    Usa PostgreSQL en lugar de SQLite. Avanza #17

--- migrations/2019-10-07-114233_create_initial_tables/up.sql
+++ migrations/2019-10-07-114233_create_initial_tables/up.sql
@@ -1,5 +1,10 @@
+create table if not exists ingredient_type (
+  id serial primary key not null,
+  type_desc varchar(50) not null
+);
+
 create table if not exists ingredient (
-  id integer primary key not null,
+  id serial primary key not null,
   name varchar(255) not null,
   energy int,
   carbohydrates int,
@@ -11,17 +16,13 @@ create table if not exists ingredient (
   foreign key (ing_type) references ingredient_type(id)
 );
 
-create table if not exists ingredient_type (
-  id integer primary key not null,
-  type_desc varchar(50) not null
-);
-
-insert or ignore into ingredient_type(id, type_desc) values (1, 'Carne'), (2, 'Pescado'), (3, 'Pasta'), (4, 'Legumbre'),
+insert into ingredient_type(id, type_desc) values (1, 'Carne'), (2, 'Pescado'), (3, 'Pasta'), (4, 'Legumbre'),
                                              (5, 'Fruta'), (6, 'Verdura'), (7, 'Condimento'), (8, 'Fruto seco'),
-					     (9, 'Graso'), (255, 'Otro');
+					     (9, 'Graso'), (255, 'Otro')
+on conflict do nothing;
 
 create table if not exists recipe (
-   id integer primary key not null,
+   id serial not null,
    name varchar(255) not null,
    preparation_time int not null check (0 < preparation_time), -- expressed in minutes
    serves int not null check (0 < serves),
@@ -29,28 +30,28 @@ create table if not exists recipe (
 );
 
 create table if not exists units (
-  id integer primary key not null,
+  id serial primary key not null,
   unit varchar(10)
 );
 
-insert or ignore into units(id, unit) values (1, 'gr'), (2, 'ml'), (3, 'unidad');
+insert into units(id, unit) values (1, 'gr'), (2, 'ml'), (3, 'unidad') on conflict do nothing;
 
 create table if not exists uses (
-  recipe integer not null,
-  ingredient integer not null,
+  recipe integer not null unique,
+  ingredient integer not null unique,
   main_ingredient int not null check (main_ingredient = 0 or main_ingredient = 1), -- whether this is a main ingredient of the recipe
 
   quantity int not null,
   unit int,
 
+  primary key (recipe, ingredient),
   foreign key (recipe) references recipe(id),
   foreign key (ingredient) references ingredient(id),
-  primary key (recipe, ingredient),
   foreign key (unit) references units(id)
 );
 
 create table if not exists menu (
-  id integer primary key not null,
+  id serial not null,
   description text
 );
 
